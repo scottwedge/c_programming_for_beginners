18627a795328b82fdee404955e2b642344978b93
Scott Wedge <scott.wedge@gmail.com>
2023-01-12 21:43:06 -0500

    NEW: C boolean statements

---
+++ bool.c
@@ -0,0 +1,15 @@
+// Boolean fooling around
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+// Main program
+
+int main() {
+    bool t = true;
+    bool f = false;
+    printf("True = %d\n", t);   // True = 1
+    printf("False = %d\n", f);  // False = 0
+}
