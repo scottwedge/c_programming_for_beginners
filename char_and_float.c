89b72c4d485433ed8eeacadf19a004f2a251030d
Scott Wedge <scott.wedge@gmail.com>
2023-01-15 22:37:16 -0500

    NEW: character %c and floating point %f

---
+++ char_and_float.c
@@ -0,0 +1,13 @@
+// Practice with characters and float numbers
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    char singleChar = 'a';
+    float floatNum = 1.999999;
+    printf("Single character is %c and floating number is %f\n", singleChar, floatNum);
+    return 0;
+}
+
