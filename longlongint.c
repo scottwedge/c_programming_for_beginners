60d8c960c1c9b6acc2164d1740cc5d79667536ee
Scott Wedge <scott.wedge@gmail.com>
2023-01-16 20:54:32 -0500

    NEW: math.h, pow(), long long integer

---
+++ longlongint.c
@@ -0,0 +1,13 @@
+// First time using long long int
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main()
+{
+    long long int lli;
+    lli = pow(2,20);
+    printf("Long long integer is %lld \n", lli);
+}
+
