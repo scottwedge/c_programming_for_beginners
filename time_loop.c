29e17d1ef1fe9f697990614c610716f0588d48f0
Scott Wedge <scott.wedge@gmail.com>
2023-02-07 20:36:12 -0500

    Change loop count from 10M to 1M

--- time_loop.c
+++ time_loop.c
@@ -9,13 +9,13 @@ int main()
 {
     int start_time = time(NULL);
     int j = 1;
-    while (j < 10000000)
+    while (j < 1000000)
     {
         printf("%d\n",j);
         j++;
     }
     int end_time = time(NULL);
     int duration = end_time - start_time;
-    printf("10M iterations took %d seconds\n",duration);
+    printf("1M iterations took %d seconds\n",duration);
 }
 
