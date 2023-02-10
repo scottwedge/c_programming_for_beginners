8435ab9e58052c837828f22ab078899b9a0b5d4b
Scott Wedge <scott.wedge@gmail.com>
2023-02-09 19:58:09 -0500

    NEW: Read user input with scanf() function

---
+++ digital_clock.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int seconds, minutes, hours;
+
+    printf("Enter seconds value:");  // Prompt for time in seconds
+    scanf("%d", &seconds);           // Read value
+
+    printf("\nSeconds value =");
+    printf("%d", seconds);            //DEBUG
+
+    return 0;
+}
