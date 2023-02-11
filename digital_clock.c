5fc4d528f7e15608f783a2354b9ab1349b697b1f
Scott Wedge <scott.wedge@gmail.com>
2023-02-10 20:44:11 -0500

    Prompt for hours, minutes and seconds in order. Add '\n' to create new lines

--- digital_clock.c
+++ digital_clock.c
@@ -4,11 +4,25 @@ int main()
 {
     int seconds, minutes, hours;
 
+    printf("Enter value in hours:");  // Prompt for Hours value
+    scanf("%d", &hours);
+
+    printf("Hours =");
+    printf("%d\n\n", hours);
+
+    printf("Enter value in minutes:");  // Prompt for minutes value
+    scanf("%d", &minutes);
+
+    printf("minutes =");
+    printf("%d\n\n", minutes);
+
+
     printf("Enter seconds value:");  // Prompt for time in seconds
     scanf("%d", &seconds);           // Read value
 
-    printf("\nSeconds value =");
-    printf("%d", seconds);            //DEBUG
+    printf("Seconds value =");     // DEBUG
+    printf("%d\n", seconds);           // DEBUG
+
 
     return 0;
 }
