0088ddf88efc34233007d47845601ad8708685fb
Scott Wedge <scott.wedge@gmail.com>
2023-02-15 22:11:37 -0500

    Remove commented  out lines

--- digital_clock.c
+++ digital_clock.c
@@ -9,22 +9,12 @@ int main()
     printf("Enter value in hours:");  // Prompt for Hours value
     scanf("%d", &hours);
 
-//    printf("Hours =");
-//    printf("%d\n\n", hours);
-
     printf("Enter value in minutes:");  // Prompt for minutes value
     scanf("%d", &minutes);
-
-//    printf("minutes =");
-//    printf("%d\n\n", minutes);
 
 
     printf("Enter seconds value:");  // Prompt for time in seconds
     scanf("%d", &seconds);           // Read value
-
-//    printf("Seconds value =");     // DEBUG
-//    printf("%d\n", seconds);           // DEBUG
-
 
 
     while (1)      // Update time forever
